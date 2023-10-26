c35611efb808cf7dc6fe69dd5d6ae5b3e5757dc6
Matejovic <matej-linux@hotmail.com>
2023-10-26 18:20:30 +0200

    now we can talk

---
+++ README.md
@@ -0,0 +1,9 @@
+You will need [OpenAI API-key](https://platform.openai.com/account/api-keys)
+
+program expets `.env` variable `OPENAI_API_KEY`. You can follow [official instructions](https://platform.openai.com/docs/quickstart?context=node).
+
+```
+bun install
+bun chat.ts "Your special prompt"
+```
+
---
+++ chat.ts
@@ -0,0 +1,18 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+async function queryBot(prompt) {
+  return  await openai.chat.completions.create({
+    messages: [{ role: "system", content: prompt}],
+    model: "gpt-3.5-turbo",
+  });
+}
+
+async function main() {
+  const input_prompt = Bun.argv[2];
+  const response = await queryBot(input_prompt);
+  console.log(response.choices[0]['message']['content']);
+}
+
+main();
